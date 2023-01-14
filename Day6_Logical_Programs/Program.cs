75da42a3fa52e1c8cb483540facd8c3b3ecaab59
vaibhav <vaibhavmehta524@gmail.com>
2023-01-14 10:26:55 +0530

    Temp conversion

--- Day6_Logical_Programs/Program.cs
+++ Day6_Logical_Programs/Program.cs
@@ -8,7 +8,7 @@ namespace LogicalPrograms
         {
             Console.WriteLine("Welcome to the logical programs");
             Console.WriteLine("Choose option from below");
-            Console.WriteLine("1.Fibbonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n8.Day Of Week");
+            Console.WriteLine("1.Fibbonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n6.StopWatch\n8.Day Of Week\n9.Temperature Conversion\n10.Monthly payment\n11.Square root");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,8 +24,21 @@ namespace LogicalPrograms
                 case 4:
                     reverseNumber.ReverseProgram(121);
                     break;
+                case 6:
+                    Watch.CalculateTime();
+                    break;
                 case 8:
                     DayOfWeek.Week();
+                    break;
+                case 9:
+                    TempratureConversion.tempCelsiusToFarheneit();
+                    TempratureConversion.tempFarheneitToCelsius();
+                    break;
+                case 10:
+                    MonthPayemnt.payments();
+                    break;
+                case 11:
+                    SquareRoot.sqrt();
                     break;
             }
             Console.ReadLine();
