4a92e9b5d7c4ed9b1f0df8ab3ca706a1bbc5ca76
vaibhav <vaibhavmehta524@gmail.com>
2023-01-14 10:04:01 +0530

    UC4_reverseNumber

---
+++ Day6_Logical_Programs/reverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class reverseNumber
+    {
+        public static void ReverseProgram(int number)
+        {
+            int remainder = 0, reverse = 0;
+
+            while (number != 0)
+            {
+                remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+
+                Console.WriteLine("remainder: {0} number: {1} reverse : {2}", remainder, number, reverse);
+            }
+
+            Console.WriteLine("Reverse number is " + reverse);
+        }
+    }
+}
